2e28e7c3ef4d512d3b398e9b5548ce8b2218f83e
Kurbanov Bulat <kurbanovbul@gmail.com>
2022-01-03 14:21:11 +0300

    Add book uploaded filter

--- fastapi_book_server/app/filters/book.py
+++ fastapi_book_server/app/filters/book.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Optional
 
 from fastapi.params import Query
@@ -6,7 +7,10 @@ from app.depends import get_allowed_langs
 
 
 def get_book_filter(
-    is_deleted: Optional[bool] = None, allowed_langs: Optional[list[str]] = Query(None)
+    is_deleted: Optional[bool] = None,
+    allowed_langs: Optional[list[str]] = Query(None),  # type: ignore
+    uploaded_gte: Optional[date] = None,
+    uploaded_lte: Optional[date] = None,
 ) -> dict:
     result = {}
 
@@ -15,5 +19,11 @@ def get_book_filter(
 
     if not (allowed_langs and "__all__" in allowed_langs):
         result["lang__in"] = get_allowed_langs(allowed_langs)
+
+    if uploaded_gte:
+        result["uploaded__gte"] = uploaded_gte
+
+    if uploaded_lte:
+        result["uploaded__lte"] = uploaded_lte
 
     return result
