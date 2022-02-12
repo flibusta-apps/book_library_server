6e307b806702841662a6d99fcd42673e5db708be
Kurbanov Bulat <kurbanovbul@gmail.com>
2022-02-12 13:05:13 +0300

    Remove aiologger

--- scripts/healthcheck.py
+++ scripts/healthcheck.py
@@ -1,11 +1,8 @@
-import os
-
 import httpx
 
 
 response = httpx.get(
     "http://localhost:8080/healthcheck",
-    headers={"Authorization": os.environ["API_KEY"]},
 )
 print(f"HEALTHCHECK STATUS: {response.status_code}")
 exit(0 if response.status_code == 200 else 1)
