e3b5cf7b7a6340f920ce46f73f86819d75fc8a50
Kurbanov Bulat <kurbanovbul@gmail.com>
2022-02-12 10:47:04 +0300

    Add metrics

--- fastapi_book_server/core/app.py
+++ fastapi_book_server/core/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 import aioredis
 from fastapi_pagination import add_pagination
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.views import routers
 from core.config import env_config
@@ -36,5 +37,7 @@ def start_app() -> FastAPI:
         database_ = app.state.database
         if database_.is_connected:
             await database_.disconnect()
+
+    Instrumentator().instrument(app).expose(app, include_in_schema=True)
 
     return app
