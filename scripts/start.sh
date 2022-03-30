6788a1de029599f3c69c1ad3302c24f0a278e7d8
Bulat Kurbanov <kurbanovbul@gmail.com>
2022-03-30 10:03:47 +0300

    Prometheus muldiproc

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,7 @@
 cd /app
+
+rm -rf prometheus
+mkdir prometheus
+
 alembic -c ./app/alembic.ini upgrade head
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app  --bind 0.0.0.0:8080
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8080
