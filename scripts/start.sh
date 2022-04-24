8c330b987d2694800602899f7f04739fba802780
Bulat Kurbanov <kurbanovbul@gmail.com>
2022-04-24 22:16:53 +0300

    Fix timeout

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,4 @@ rm -rf prometheus
 mkdir prometheus
 
 alembic -c ./app/alembic.ini upgrade head
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8080
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8080 --timeout 600
