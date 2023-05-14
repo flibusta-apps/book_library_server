ec42858efac11aeefd8a37b575c250c95ec40ee3
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-05-15 00:34:22 +0200

    Update timeout

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,4 @@ rm -rf prometheus
 mkdir prometheus
 
 alembic -c ./app/alembic.ini upgrade head
-uvicorn main:app --host 0.0.0.0 --port 8080 --loop uvloop --workers 2
+uvicorn main:app --host 0.0.0.0 --port 8080 --loop uvloop --workers 2 --timeout-keep-alive 30
