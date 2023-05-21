b738355f7cab1f02d63eede36356c2a010fa1adb
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-05-21 02:18:42 +0200

    Fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ mkdir prometheus
 
 alembic -c ./app/alembic.ini upgrade head
 
-gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8080 --timeout 30 --max-requests=512000 --max-requests-jitter=3
+gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8080 --workers=2 --timeout 30 --max-requests=512000 --max-requests-jitter=3
