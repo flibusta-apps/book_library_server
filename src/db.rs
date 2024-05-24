eec1c770714d39e80412c1fdadbd0b1be452e201
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-05-24 13:59:28 +0200

    Set pool_timeout to 300

--- src/db.rs
+++ src/db.rs
@@ -2,7 +2,7 @@ use crate::{config::CONFIG, prisma::PrismaClient};
 
 pub async fn get_prisma_client() -> PrismaClient {
     let database_url: String = format!(
-        "postgresql://{}:{}@{}:{}/{}?connection_limit=10",
+        "postgresql://{}:{}@{}:{}/{}?connection_limit=10&pool_timeout=300",
         CONFIG.postgres_user,
         CONFIG.postgres_password,
         CONFIG.postgres_host,
